746497e92e05f843692ee71a58c84102aae619f4
syumai <syumai@gmail.com>
2016-09-13 02:13:18 +0900

    Added asset roots

--- lib/yabass/assets/router.rb
+++ lib/yabass/assets/router.rb
@@ -1,14 +1,20 @@
 module Yabass
   module Assets
     module Router
-      attr_reader :assets
+      attr_reader :assets, :asset_roots
       def initialize(*args, &block)
-        init_asset_routes
+        init_asset_roots
+        init_assets
         super
       end
 
       private
-        def init_asset_routes
+        def init_asset_roots
+          dir = Dir.open('assets')
+          @asset_roots = dir.to_a.select{|name| /^\./ !~ name }
+        end
+
+        def init_assets
         end
     end
   end
