7a43377c69e1db6c8bb49d1a974a08913645e874
syumai <syumai@gmail.com>
2016-12-11 15:18:33 +0900

    Updated server
    Added support for static file serving

--- lib/yabass/server.rb
+++ lib/yabass/server.rb
@@ -9,11 +9,21 @@ module Yabass
                                            BindAddress: '127.0.0.1',
                                            Port: 3030})
         server.mount_proc('/') do |req, res|
-          found_page = pages.find{|page| page.route === req.path }
+          found_page = pages.find{|page| /^#{page.route}\/?$/ =~ req.path }
+          file_path = File.expand_path("public/#{req.path}", ::Yabass::root)
           if found_page
             res.body = Renderer.render(found_page.file_path, found_page.data, found_page.parent)
+            res.content_type = 'text/html'
+            res.status = 200
+          elsif File.file?(file_path)
+            res.body = File.read(file_path)
+            mime = WEBrick::HTTPUtils.mime_type(file_path, WEBrick::HTTPUtils::DefaultMimeTypes)
+            res.content_type = mime
+            res.status = 200
           else
             res.body = "Path: #{req.path}\nNot found"
+            res.content_type = 'text/plain'
+            res.status = 404
           end
         end
         trap("INT"){ server.shutdown }
