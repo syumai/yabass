f892c8cc28a2b79f0127d681052bde057f5e6515
syumai <syumai@gmail.com>
2017-02-14 20:16:55 +0900

    Added filenames to ERB in Renderer

--- lib/yabass/renderer.rb
+++ lib/yabass/renderer.rb
@@ -3,6 +3,7 @@ module Yabass
     class << self
       def render(full_file_path, data = nil, parent = nil, layout = '_layout.erb')
         view_erb = ERB.new(File.read(full_file_path))
+        view_erb.filename = full_file_path
         page = view_erb.result(binding)
         render_layout(page)
       end
@@ -10,15 +11,16 @@ module Yabass
       def render_partial(file_path, **args)
         full_file_path = File.expand_path("views/#{file_path}", ::Yabass::root)
         view_erb = ERB.new(File.read(full_file_path))
+        view_erb.filename = full_file_path
         _binding = binding
         args.each {|k, v| _binding.local_variable_set(k, v) } unless args.empty?
         view_erb.result(_binding)
       end
 
       def render_layout(page)
-        layout_path = 'views/_layout.erb'
-        layout_file = File.expand_path(layout_path, ::Yabass::root)
-        layout_erb = ERB.new(File.read(layout_file))
+        full_layout_path = File.expand_path("views/_layout.erb", ::Yabass::root)
+        layout_erb = ERB.new(File.read(full_layout_path))
+        layout_erb.filename = full_layout_path
         layout_erb.result(binding)
       end
     end
